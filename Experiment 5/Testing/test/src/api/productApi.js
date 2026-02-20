9ab8a69e496048d9a2255deb6307d80960b1b98e
Advitiya Sharma <official.advitiya@gmail.com>
2026-02-20 14:09:42 +0530

    Delete Experiment 5 directory

--- Experiment 5/Testing/test/src/api/productApi.js
+++
@@ -1,7 +0,0 @@
-export const fetchProduct = async () => {
-  return {
-    id: 1,
-    name: "Laptop",
-    price: 5000,
-  };
-};
--- Experiment 5/Testing/test/src/components/Welcome.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const Welcome = ({ name }) => {
-  return <h2>Welcome, {name}</h2>;
-};
-
-export default Welcome;
